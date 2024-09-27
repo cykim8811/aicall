efe7923ac633dea03a87336acccbdb8043c3c8bc
cykim8811 <cykim8811@snu.ac.kr>
2024-09-27 07:04:41 +0000

    threading

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 from pyVoIP.VoIP import VoIPPhone, CallState
-
 from rtzr import RTZRClient
-
 import wave
 import dotenv
 import time
 import os
-
+import threading
 
 dotenv.load_dotenv()
 
 PASSWORD = os.environ.get("PASSWORD")
 
+f = wave.open('./GREETINGS.wav', 'rb')
+greeting_audio = f.readframes(f.getnframes())
+f.close()
+
+def run_reader(call):
+    while call.state == CallState.ANSWERED:
+        data = call.read_audio()
+        if data:
+            call.write_audio(data)
+            print(">", end="", flush=True)
+
 def answer(call):
-    try:
-        f = wave.open('./GREETINGS.wav', 'rb')
-        data = f.readframes(f.getnframes())
-        f.close()
+    call.answer()
+    call.write_audio(greeting_audio)
+    time.sleep(5)
 
-        call.answer()
-        call.write_audio(data)
+    reader = threading.Thread(target=run_reader, args=(call,))
+    reader.start()
 
-        time.sleep(3)
-        while call.state == CallState.ANSWERED:
-            data = call.read_audio()
-            print(len(data))
+    while call.state == CallState.ANSWERED:
+        time.sleep(1)
 
-    except Exception as e:
-        print(e)
-    finally:
-       print("Hangup call")
-       call.hangup()
-
+    call.hangup()
 
 vp = VoIPPhone(
     'london1.voip.ms', 5060, '420319_ai', 
